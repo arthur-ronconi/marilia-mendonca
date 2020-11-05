f28be2ba64bf4379474c81e1465fda673b864ab5
Arthur Ronconi <arthur.ronconi@gmail.com>
2020-11-04 21:02:42 -0300

    disclaimer link

--- sections/disclaimer.jsx
+++ sections/disclaimer.jsx
@@ -6,7 +6,13 @@ export default function Disclaimer() {
           <div className="row d-flex justify-content-center">
             <div className="text-center">
               Esse site <strong>não é oficial</strong>.
-              <br className="d-md-none" /> Clique <a href="#">AQUI</a>{" "}
+              <br className="d-md-none" /> Clique{" "}
+              <a
+                href="http://www.mariliamendoncaoficial.com.br/"
+                rel="noopener noreferrer"
+              >
+                AQUI
+              </a>{" "}
               <br className="d-md-none" />
               para acessar o site oficial
             </div>
