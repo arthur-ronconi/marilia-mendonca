84af8dcc1032d6c9147909d32beaaf9ce2648c43
Arthur Ronconi <arthur.ronconi@gmail.com>
2023-04-14 18:25:14 -0300

    next update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import "../styles/globals.scss";
 
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import * as gtag from "../lib/gtag";
 
 const App = ({ Component, pageProps }) => {
@@ -17,7 +18,29 @@ const App = ({ Component, pageProps }) => {
     };
   }, [router.events]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
